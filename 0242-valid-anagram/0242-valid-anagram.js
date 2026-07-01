9c266e39fcde789d00875791f424877fc5fe2531
Annt <136672470+Anant23452@users.noreply.github.com>
2026-07-01 22:26:45 +0530

    Time: 11 ms (77.64%), Space: 55 MB (86.52%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    let map ={};
+    if(s.length !== t.length)return false;
+    for(let i =0;i<s.length;i++){
+        if(!map[s[i]]){
+            map[s[i]]=1;
+        }
+        else{
+            map[s[i]]++
+        }
+    }
+    for(let i =0;i<t.length;i++){
+        if(map[t[i]]==undefined || map[t[i]]==0){
+            return false;
+        }
+        else{
+            --map[t[i]]
+        }
+    }
+    return true;
+};
