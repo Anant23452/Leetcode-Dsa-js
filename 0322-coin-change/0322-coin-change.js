a683f53001be7e809b77cc467ae9d15fc6a08f49
Annt <136672470+Anant23452@users.noreply.github.com>
2026-07-10 17:36:00 +0530

    Time: 31 ms (85.28%), Space: 59.8 MB (63.68%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} coins
+ * @param {number} amount
+ * @return {number}
+ */
+var coinChange = function(coins, amount) {
+    let n = coins.length;
+    let dp = new Array(amount+1).fill(Infinity);
+    // base case
+    dp[0]=0
+    for(let i =1;i<=amount;i++){
+       
+        for(let j =0;j<n;j++){
+            let spendamt = i - coins[j];
+             if(spendamt>=0){
+                dp[i]=Math.min(dp[i],dp[spendamt]+1)
+             }
+        }
+    }
+    return dp[amount]===Infinity?-1:dp[amount]
+};
