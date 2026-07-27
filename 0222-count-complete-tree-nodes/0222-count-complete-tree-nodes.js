b4a98bcc44074de27b35d87b6a64f02a9fafd347
Anant kumar patel <136672470+Anant23452@users.noreply.github.com>
2026-07-27 18:11:24 +0530

    Time: 1 ms (40.54%), Space: 75 MB (23.96%) - LeetHub

---
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.js
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var countNodes = function(root) {
+    function dfs(node){
+        if(!node)return 0;
+       let left= dfs(node.left);
+       let right= dfs(node.right)
+        return left+right+1;
+    }
+    return dfs(root)
+};
