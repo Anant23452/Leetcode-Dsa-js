645d8816f7fab5f62c631b8aacf0e78e293aa4a3
Anant kumar patel <136672470+Anant23452@users.noreply.github.com>
2026-07-29 23:27:02 +0530

    Time: 2 ms (84.62%), Space: 59.7 MB (57.69%) - LeetHub

---
+++ 1218-lowest-common-ancestor-of-deepest-leaves/1218-lowest-common-ancestor-of-deepest-leaves.js
@@ -0,0 +1,53 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var lcaDeepestLeaves = function (root) {
+
+    let dfs = (node) => {
+        if (!node) {
+            return {
+                height: 0,
+                node: null
+            };
+        }
+        let left = dfs(node.left);
+        let right = dfs(node.right);
+       
+        if (left.height > right.height) {
+            return {
+                height: left.height + 1,
+                node: left.node
+            }
+
+        }
+        else if (left.height < right.height) {
+            return {
+                height: right.height + 1,
+                node: right.node
+            }
+
+        } else {
+            return {
+                height: left.height + 1,
+                node: node
+            }
+
+
+        }
+        
+        
+
+
+    }
+    return dfs(root).node
+
+};
